699f69601d7c82e1359d3ae86019f7440202d66d
songrit - os x <song.cpe@gmail.com>
2016-06-12 21:50:38 +0700

    success : training php class

--- 1.class/Student.php
+++ 1.class/Student.php
@@ -1,15 +1,23 @@
+<!-- Example from : http://code.tutsplus.com -->
+
 <?php
 	class Student{
-		private $name = "";
+		private $name = "null";
 		private $university = "null";
-
+		private $major = "null";
 		public function Student($name) {
 			$this -> name = $name;
 		}
 
-// *** this is constructor form ***
+// *** this is constructor php form ***
 		// public function __construct() {
 		// 	echo "This class name is" . " " . __CLASS__ . "<br/>";
+		// }
+
+// **** this is destructor php form ***
+// *** active when end file php ***
+		// public function __destruct() {
+		// 	echo "<br/>" . 'The class "', __CLASS__, '" was destroyed' . "<br/>";
 		// }
 
 		public function setName($name) {
@@ -27,14 +35,39 @@
 		public function getUniversity() {
 			return $this -> university . "<br/>";
 		}
+
+		public function setMajor($major) {
+			$this -> major = $major;
+		}
+
+		public function getMajor() {
+			return $this -> major . "<br/>";
+		}
 	}
 
 	$jame = new Student("Songrit Maleerat");
-	// $jame -> setName("Songrit Maleerat"); 
 	$jame -> setUniversity("Suranaree University Of Technology");
-
+	$jame -> setMajor("Conputer Engineering");
 	//var_dump($s);
 
-	echo "My name is " . ($jame -> getName());
-	 echo "My University name is " . ($jame -> getUniversity());
+	echo "Student name: " . ($jame -> getName());
+	echo "University: " . ($jame -> getUniversity());
+	echo "Major: " . ($jame -> getMajor());
+
+	echo "<br/>";
+
+	$jib = new Student("Patinya Maleerat");
+	$jib -> setUniversity("Khonkaen University");
+	$jib -> setMajor("Faculty of Pharmaceutical");
+
+
+	echo "Student name: " . ($jib -> getName());
+	echo "University: " . ($jib -> getUniversity());
+	echo "Major: " . ($jib -> getMajor());
+	
+// *** Destroy the object ***
+	unset($jame);
+	unset($jib);
+
+
 ?>
